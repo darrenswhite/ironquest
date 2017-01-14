b913fa7fa7f74ac56904556e4dd4a580d5057b0c
Darren White <darrenswhite95@gmail.com>
2017-01-14 17:52:46 +0000

    Improved LampAction.getMessage

--- src/main/java/com/darrenswhite/rs/ironquest/action/LampAction.java
+++ src/main/java/com/darrenswhite/rs/ironquest/action/LampAction.java
@@ -5,6 +5,7 @@ import com.darrenswhite.rs.ironquest.player.Skill;
 import com.darrenswhite.rs.ironquest.quest.Lamp;
 import com.darrenswhite.rs.ironquest.quest.Quest;
 
+import java.util.Iterator;
 import java.util.Objects;
 import java.util.Set;
 
@@ -55,8 +56,33 @@ public class LampAction extends Action {
 
 	@Override
 	public String getMessage() {
+		StringBuilder message = new StringBuilder();
 		String xp = Skill.formatXP(lamp.getValue());
-		return "Use " + xp + " xp lamp on " + skills;
+
+		message.append("Use ").append(xp).append(" xp lamp on ");
+
+		Iterator<Skill> it = skills.iterator();
+
+		// Add the first skill
+		if (it.hasNext()) {
+			message.append(it.next());
+		}
+
+		// Append remaining skills separated by
+		// a comma (or `and` for the last Skill)
+		while (it.hasNext()) {
+			Skill s = it.next();
+
+			if (it.hasNext()) {
+				message.append(", ");
+			} else {
+				message.append(" and ");
+			}
+
+			message.append(s);
+		}
+
+		return message.toString();
 	}
 
 	/**
