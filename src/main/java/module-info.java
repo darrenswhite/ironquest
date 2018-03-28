a996c2aaebf4d73b45290d36e3d17685eb769e84
Darren White <darrenswhite95@gmail.com>
2018-03-28 16:04:59 +0100

    Update module-info.java

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -7,6 +7,8 @@ module IronQuest {
 	requires commons.csv;
 	requires javafx.swing;
 	requires java.sql;
+	exports com.darrenswhite.rs.ironquest;
 	exports com.darrenswhite.rs.ironquest.quest;
+	opens com.darrenswhite.rs.ironquest;
 	opens com.darrenswhite.rs.ironquest.quest;
 }
