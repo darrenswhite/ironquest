ed0c5cd086be6f3661379bc01c016b1a68ddd8f5
Darren White <darrenswhite95@gmail.com>
2019-07-21 11:49:11 +0100

    Add ApplicationTest

---
+++ src/test/java/com/darrenswhite/rs/ironquest/ApplicationTest.java
@@ -0,0 +1,16 @@
+package com.darrenswhite.rs.ironquest;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = {Application.class})
+public class ApplicationTest {
+
+  @Test
+  public void main() {
+    Application.main(new String[0]);
+  }
+}
