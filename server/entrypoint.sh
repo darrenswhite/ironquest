508919016b5092155589421a9d296e5c1b0229f0
Darren White <darrenswhite95@gmail.com>
2020-01-29 20:10:44 +0000

    Optimise Dockerfile

--- server/entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
-pushd ${SCRIPT_DIR} >/dev/null
-
-mvn -T 1C clean package -Dmaven.test.skip=true
-
-mvn spring-boot:run
