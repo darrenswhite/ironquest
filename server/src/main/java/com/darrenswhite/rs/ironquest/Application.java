5eba8b87dcf2fbb91a77c4eb2dd3c215b289b1c9
Darren White <darrenswhite95@gmail.com>
2020-01-29 16:36:14 +0000

    Add cors configuration

--- server/src/main/java/com/darrenswhite/rs/ironquest/Application.java
+++ server/src/main/java/com/darrenswhite/rs/ironquest/Application.java
@@ -2,6 +2,9 @@ package com.darrenswhite.rs.ironquest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 /**
  * Spring application class.
@@ -13,5 +16,16 @@ public class Application {
 
   public static void main(String[] args) {
     SpringApplication.run(Application.class, args);
+  }
+
+  @Bean
+  public WebMvcConfigurer corsConfigurer() {
+    return new WebMvcConfigurer() {
+
+      @Override
+      public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**");
+      }
+    };
   }
 }
