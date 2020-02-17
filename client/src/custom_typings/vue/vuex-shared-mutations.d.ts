25e82fae958ce7780ff55ea6f79130846ebfb046
Darren White <darrenswhite95@gmail.com>
2020-02-17 22:38:03 +0000

    Move vuex-shared-mutations typings to separate file

---
+++ client/src/custom_typings/vue/vuex-shared-mutations.d.ts
@@ -0,0 +1,35 @@
+declare module 'vuex-shared-mutations' {
+  import { Plugin } from 'vuex';
+
+  export type Predicate =
+    | string[]
+    | ((mutation: { type: string; payload: unknown }, state: unknown) => void);
+
+  export interface Strategy {
+    addEventListener(fn: Function): unknown;
+    share(message: unknown): unknown;
+  }
+
+  export interface Options {
+    predicate: Predicate;
+    strategy?: Strategy;
+  }
+
+  export default function createMutationsSharer(
+    options: Options
+  ): Plugin<unknown>;
+
+  export class BroadcastChannelStrategy implements Strategy {
+    static available(): boolean;
+    constructor(options: { key: string });
+    addEventListener(fn: Function): unknown;
+    share(message: unknown): unknown;
+  }
+
+  export class LocalStorageStratery implements Strategy {
+    static available(): boolean;
+    constructor(options: { key: string });
+    addEventListener(fn: Function): unknown;
+    share(message: unknown): unknown;
+  }
+}
