9d1e855085ff3d66f9e5c91515cd9c79acb0f45f
Darren White <darrenswhite95@gmail.com>
2020-02-17 20:49:14 +0000

    Add overwolf build script

---
+++ client/overwolf.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -e
+
+TARGET=overwolf API=external npm run compile
+
+TMP_DIR="/tmp/ironquest"
+APP_DIR="${TMP_DIR}/app"
+STORE_DIR="${TMP_DIR}/store"
+ZIP_FILE="ironquest.zip"
+
+rm -rf "${TMP_DIR}"
+rm -f "${ZIP_FILE}"
+mkdir -p "${APP_DIR}" "${STORE_DIR}"
+
+cp -r manifest.json assets/ build/ "${APP_DIR}"
+cp -r assets/logo-258-198.png assets/icon-55x55.png assets/screenshot*.png description.txt store.json "${STORE_DIR}"
+
+pushd "${TMP_DIR}"
+zip -r "${ZIP_FILE}" .
+popd
+mv "${TMP_DIR}/${ZIP_FILE}" .
+rm -rf "${TMP_DIR}"
