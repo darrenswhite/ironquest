53012d4c1c19eab0f84339a1820c5ad4613f5939
Darren White <darrenswhite95@gmail.com>
2020-03-29 10:40:35 +0100

    Update overwolf build script

--- client/overwolf/build.sh
+++ client/overwolf/build.sh
@@ -13,8 +13,16 @@ rm -rf "${TMP_DIR}"
 rm -f "${ZIP_FILE}"
 mkdir -p "${APP_DIR}" "${STORE_DIR}"
 
+convert ../assets/logo-258x198.png ../assets/Tile.jpg
+convert ../assets/icon-55x55.png ../assets/Icon.jpg
+convert ../assets/screenshot-username.png ../assets/Screenshot1.jpg
+convert ../assets/screenshot-actions-horizontal.png ../assets/Screenshot2.jpg
+convert ../assets/screenshot-actions-vertical.png ../assets/Screenshot3.jpg
+convert ../assets/screenshot-settings.png ../assets/Screenshot4.jpg
+
 cp -r manifest.json build/ "${APP_DIR}"
-cp -r ../assets/logo-258x198.png ../assets/icon-55x55.png ../assets/screenshot*.png description.txt store.json "${STORE_DIR}"
+mv ../assets/*.jpg "${STORE_DIR}"
+cp -r description.html store.json "${STORE_DIR}"
 
 pushd "${TMP_DIR}"
 zip -r "${ZIP_FILE}" .
