4bc62bb72c92ff7590a1120337f926452498ce22
Darren White <darrenswhite95@gmail.com>
2020-04-05 10:06:59 +0100

    Add navigation drawer

--- client/src/main.ts
+++ client/src/main.ts
@@ -3,6 +3,7 @@ import Vue, {VueConstructor} from 'vue';
 import Vuetify from 'vuetify/lib';
 import colors from 'vuetify/lib/util/colors';
 
+import '@mdi/font/css/materialdesignicons.css';
 import '@/styles/base.scss';
 
 Vue.config.productionTip = false;
--- client/src/web/App.vue
+++ client/src/web/App.vue
@@ -1,8 +1,32 @@
 <template>
   <v-app v-cloak>
     <v-app-bar app>
+      <v-app-bar-nav-icon @click="drawer = !drawer" />
       <v-toolbar-title>IronQuest</v-toolbar-title>
     </v-app-bar>
+    <v-navigation-drawer
+      v-model="drawer"
+      absolute
+      temporary
+    >
+      <v-list dense>
+        <v-list-item
+          v-for="(item, index) in items"
+          :key="index"
+          :href="item.link"
+          target="_blank"
+          link
+        >
+          <v-list-item-icon>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-icon>
+
+          <v-list-item-content>
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
     <v-content>
       <v-container fluid>
         <v-row>
@@ -53,10 +77,23 @@ import Settings from '@/components/Settings.vue';
 import {constants} from '@/store';
 import {mapFields} from 'vuex-map-fields';
 
+const GITHUB_URL = 'https://github.com/darrenswhite/ironquest';
+const PAYPAL_URL =
+  'https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=ZJZ94RDWF6GU4&item_name=IronQuest&currency_code=GBP&source=url';
+
 export default Vue.extend({
   components: {
     Actions,
     Settings,
+  },
+  data() {
+    return {
+      drawer: false,
+      items: [
+        {title: 'GitHub', icon: 'mdi-github', link: GITHUB_URL},
+        {title: 'Donate', icon: 'mdi-currency-usd-circle', link: PAYPAL_URL},
+      ],
+    };
   },
   computed: mapFields(['actions.loading']),
   methods: {
