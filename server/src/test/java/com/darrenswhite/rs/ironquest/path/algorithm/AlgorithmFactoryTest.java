a0425a6e1e86bd65b17d218d8b0eb077b826010e
Darren White <darrenswhite95@gmail.com>
2020-05-27 16:07:32 +0100

    Add AlgorithmFactoryTest

---
+++ server/src/test/java/com/darrenswhite/rs/ironquest/path/algorithm/AlgorithmFactoryTest.java
@@ -0,0 +1,27 @@
+package com.darrenswhite.rs.ironquest.path.algorithm;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+
+import java.util.Set;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+class AlgorithmFactoryTest {
+
+  @Nested
+  class GetAlgorithm {
+
+    @Test
+    void shouldReturnCorrectAlgorithmGivenId() {
+      DefaultAlgorithm defaultAlgorithm = new DefaultAlgorithm();
+      SmartPriorities smartPriorities = new SmartPriorities();
+      AlgorithmFactory algorithmFactory = new AlgorithmFactory(
+          Set.of(defaultAlgorithm, smartPriorities));
+
+      assertThat(algorithmFactory.getAlgorithm(AlgorithmId.DEFAULT), equalTo(defaultAlgorithm));
+      assertThat(algorithmFactory.getAlgorithm(AlgorithmId.SMART_PRIORITIES),
+          equalTo(smartPriorities));
+    }
+  }
+}
